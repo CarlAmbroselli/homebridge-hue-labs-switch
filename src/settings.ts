8efbc8f37d28b2923707c287592e2e99571b6f20
Carl Ambroselli <git@carl-ambroselli.de>
2021-03-07 17:58:10 +0100

    Rename package

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'HueLabs';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-hue-labs';
+export const PLUGIN_NAME = 'homebridge-hue-labs-switch';
