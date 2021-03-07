e99600f7ce83a16872474b03b58c6cdf98c687ec
Carl Ambroselli <git@carl-ambroselli.de>
2021-03-07 13:35:54 +0100

    Initial commit

---
+++ src/platformAccessory.ts
@@ -0,0 +1,77 @@
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+
+import { HueLabsHomebridgePlatform } from './platform';
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class HueLabsAccessory {
+  private service: Service;
+
+  private states = {
+    On: false
+  };
+
+  constructor(
+    private readonly platform: HueLabsHomebridgePlatform,
+    private readonly accessory: PlatformAccessory,
+  ) {
+
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+
+    this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
+
+    // service name, this is what is displayed as the default name on the Home app
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+
+    // each service must implement at-minimum the "required characteristics" for the given service type
+    // see https://developers.homebridge.io/#/service/Switch
+
+    // register handlers for the On/Off Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
+      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+  }
+
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, turning on the switch.
+   */
+  async setOn(value: CharacteristicValue) {
+    // implement your own code to turn your device on/off
+    this.states.On = value as boolean;
+
+    this.platform.log.debug('Set Characteristic On ->', value);
+  }
+
+  /**
+   * Handle the "GET" requests from HomeKit
+   * These are sent when HomeKit wants to know the current state of the accessory, when checking if the Switch is on.
+   *
+   * GET requests should return as fast as possbile. A long delay here will result in
+   * HomeKit being unresponsive and a bad user experience in general.
+   *
+   * If your device takes time to respond you should update the status of your device
+   * asynchronously instead using the `updateCharacteristic` method instead.
+
+   * @example
+   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+   */
+  async getOn(): Promise<CharacteristicValue> {
+    const isOn = this.states.On;
+
+    this.platform.log.debug('Get Characteristic On ->', isOn);
+
+    // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+
+    return isOn;
+  }
+
+}
